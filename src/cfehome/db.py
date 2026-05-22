7b7528bf873473e5eb5abb80479057feb910bd73
kepin <kevin.aldriks@gmail.com>
2026-05-22 16:00:02 +0700

    y

--- src/cfehome/db.py
+++ src/cfehome/db.py
@@ -1,25 +1,36 @@
 from cfehome.env import config
+import dj_database_url
 
-POSTGRES_USER = config("POSTGRES_USER", default=None)
-POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", default=None)
-POSTGRES_DB = config("POSTGRES_DB", default=None)
-POSTGRES_HOST = config("POSTGRES_HOST", default=None)
-POSTGRES_PORT = config("POSTGRES_PORT", default=None)
-POSTGRES_DB_IS_AVAIL = all(
-    [POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT]
-)
-POSTGRES_DB_REQUIRE_SSL = config("POSTGRES_DB_REQUIRE_SSL", cast=bool, default=False)
-
-if POSTGRES_DB_IS_AVAIL:
+DATABASE_URL = config("DATABASE_URL", default=None)
+if DATABASE_URL is not None:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": POSTGRES_DB,
-            "USER": POSTGRES_USER,
-            "PASSWORD": POSTGRES_PASSWORD,
-            "HOST": POSTGRES_HOST,
-            "PORT": POSTGRES_PORT,
-        }
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-    if POSTGRES_DB_REQUIRE_SSL:
-        DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
+else:
+    POSTGRES_USER = config("POSTGRES_USER", default=None)
+    POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", default=None)
+    POSTGRES_DB = config("POSTGRES_DB", default=None)
+    POSTGRES_HOST = config("POSTGRES_HOST", default=None)
+    POSTGRES_PORT = config("POSTGRES_PORT", default=None)
+    POSTGRES_DB_IS_AVAIL = all(
+        [POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT]
+    )
+    POSTGRES_DB_REQUIRE_SSL = config("POSTGRES_DB_REQUIRE_SSL", cast=bool, default=False)
+
+    if POSTGRES_DB_IS_AVAIL:
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": POSTGRES_DB,
+                "USER": POSTGRES_USER,
+                "PASSWORD": POSTGRES_PASSWORD,
+                "HOST": POSTGRES_HOST,
+                "PORT": POSTGRES_PORT,
+            }
+        }
+        if POSTGRES_DB_REQUIRE_SSL:
+            DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
